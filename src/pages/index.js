f5453ef55fce0640c9fa97d64f236e6068cb49b2
mjocc <61629073+mjocc@users.noreply.github.com>
2021-11-19 21:10:04 +0000

    Update header text on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import SocialMediaIcons from '@components/SocialMediaIcons';
 
 import utils from '@styles/Utilities.module.scss';
 
+const HeaderText = ({ className = '', children }) => <h1 className={`text-white font-heading text-8xl ${className}`}>{children}</h1>
+
 export default function Home() {
   return (
     <>
@@ -18,9 +20,10 @@ export default function Home() {
       <MainContent>
         <div className={`${utils.flexCenter} sm:w-screen sm:h-screen`}>
           <div className="fade flex flex-col">
-            <h1 className="text-white font-heading text-8xl">Hi, I'm</h1>
-            <h1 className="text-white font-heading text-9xl">Matthew</h1>
-            <h2 className="text-white font-heading text-5xl">Web developer</h2>
+            <HeaderText>Hi,</HeaderText>
+            <HeaderText>I'm Matthew,</HeaderText>
+            <HeaderText>Web developer</HeaderText>
+          
             <SocialMediaIcons className="mt-5 pl-2" width="350px" />
           </div>
         </div>
