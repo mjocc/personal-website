338d38a79bcbd68db459e035e7f30de67f88e036
mjocc <61629073+mjocc@users.noreply.github.com>
2021-11-20 17:24:36 +0000

    Add netlify deploy history to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 const FooterLink = ({ href, children }) => (
-  <a href={href} className="text-gray-100 hover:underline text-sm">
+  <a href={href} className="text-sm text-gray-100 hover:underline">
     {children}
   </a>
 );
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 flex items-center justify-between px-3 py-2 w-full bg-gray-900">
+    <footer className="absolute bottom-0 flex items-center justify-between w-full px-3 py-2 bg-gray-900">
       <FooterLink href="https://www.github.com/mjocc/personal-website">
         GitHub Repository
       </FooterLink>
-      <FooterLink href="https://www.netlify.com/">
+      <FooterLink href="https://app.netlify.com/sites/priceless-liskov-5ad496/deploys">
         Powered by Netlify
       </FooterLink>
     </footer>
