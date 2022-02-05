7a0f6e35faf55a5caac43e24f9ca29283768dcfb
mjocc <61629073+mjocc@users.noreply.github.com>
2022-02-05 22:22:56 +0000

    Add site config validation

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,6 @@
-import siteConfig from 'content/config.json';
+import config from 'content/config.json';
 import { createContext } from 'react';
 import { z } from 'zod';
-
-// TODO: add validation here
 
 const configSchema = z.object({
   twitter_account: z.string(),
@@ -12,6 +10,7 @@ const configSchema = z.object({
   site_description: z.string(),
   site_keywords: z.string().array(),
 });
-type SiteConfig = z.infer<typeof configSchema>;
 
-export default siteConfig as SiteConfig;
+const siteConfig = configSchema.parse(config)
+
+export default siteConfig;
