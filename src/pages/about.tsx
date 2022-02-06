c54fdf491a61a1747fcd194a920da99efec97817
mjocc <61629073+mjocc@users.noreply.github.com>
2022-02-06 15:01:57 +0000

    Add cv todo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,8 @@ import Layout from '@components/structure/Layout';
 import { NextPage } from 'next';
 import Head from 'next/head';
 
+// TODO: Add some kind of cv link (mabye google drive link/embed or netlify hosted file etc.)
+
 const AboutPage: NextPage = () => (
   <Layout>
     <Head>
